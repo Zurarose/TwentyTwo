7751cf0bbdb6af98410e55fef65d2e09621e2bc9
Олег Санников <specter8471102@gmail.com>
2021-10-15 15:54:26 +0300

    debug activate

--- TwentyTwo/settings.py
+++ TwentyTwo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b3cb972c-8444-4804-95bd-89c95c882ac7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SESSION_COOKIES_SAMESITE = "None"
 
